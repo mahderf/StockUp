dcae253851ac301ed1db842aa31c4db24ff7222a
nmerris <nate.merris@gmail.com>
2017-08-23 16:38:50 -0400

    attempting to add very basic logout functionality

--- src/main/java/com/stockup/group/SecurityConfiguration.java
+++ src/main/java/com/stockup/group/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -16,13 +17,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 // allow anyone to access default route, home, both purchase pages
                 .antMatchers("/", "/home", "/purchaseproduct", "/purchaseproductconfirmation",
                         "/css/**", "/js/**", "/img/**", "/scss/**", "/vendor/**").permitAll() // to allow css and js files to work, could also add img folders, etc
-                .anyRequest().authenticated()
-                .and()
+                .anyRequest().authenticated() // after authentication, all routes will be accessible
+
                 // below: this just points to the page that will SHOW when user tries to access any route that requires auth
                 // after successful login, they will automatically continue to whatever link they clicked to trigger the auth form
-                .formLogin().loginPage("/login").permitAll()
-                .and()
-                .httpBasic();
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().httpBasic() // allows authentication in the URL itself
+                // logout via httprequest, not very secure but ok for now
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));;
     }
 
     @Override
--- src/main/java/com/stockup/group/controller/MainController.java
+++ src/main/java/com/stockup/group/controller/MainController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.text.Format;
@@ -40,6 +37,11 @@ public class MainController {
     @PostMapping("/login")
     public String loginPost() {
         return "login";
+    }
+
+    @RequestMapping("/logout")
+    public String logout() {
+        return "logout";
     }
 
 
